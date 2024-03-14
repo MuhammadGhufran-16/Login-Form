844b5c13bbcb9ea442b2e8be45f806336c4d6f96
Mohamed Imran <anismuhammad280@gmail.com>
2024-03-14 13:15:12 +0530

    code committed

---
+++ login/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+import React from 'react';
+import LoginForm from './LoginForm';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to My App</h1>
+      <LoginForm />
+    </div>
+  );
+};
+
+export default App;
---
+++ login/src/LoginForm.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+
+const LoginForm = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Here you can perform authentication logic, like sending a request to a server
+    console.log('Username:', username);
+    console.log('Password:', password);
+    // Reset the form fields after submission
+    setUsername('');
+    setPassword('');
+  };
+
+  return (
+    <div>
+      <h2>Login form</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={handleUsernameChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={handlePasswordChange}
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
